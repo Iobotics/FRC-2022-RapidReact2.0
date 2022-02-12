c3348b166a219d39638f18d21f903e502995603b
Spryzen <bkt2401@iolani.org>
2022-02-11 16:47:33 -1000

    Updated PID Testign

--- FRC-2022-RapidReact2.0/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2022-RapidReact2.0/src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -31,6 +34,11 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    SmartDashboard.putNumber("Distance",10);
+    SmartDashboard.putNumber("P",0);
+    SmartDashboard.putNumber("I",0);
+    SmartDashboard.putNumber("D",0);
     // Configure the button bindings
     drivetrain.setDefaultCommand(new RunCommand(
       () -> drivetrain.drive.tankDrive(-joystick1.getY(), joystick2.getY()), drivetrain));
@@ -45,7 +53,16 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(joystick2,1).whileHeld(
+      new StartEndCommand(
+        ()-> drivetrain.motionMagic(SmartDashboard.getNumber("Distance", 0), 10,SmartDashboard.getNumber("P", 0),SmartDashboard.getNumber("I", 0),SmartDashboard.getNumber("D", 0)),
+        ()-> drivetrain.stop(),drivetrain
+      )
+    );
+    
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- FRC-2022-RapidReact2.0/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ FRC-2022-RapidReact2.0/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,8 +5,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 /** Add your docs here. */
@@ -18,11 +25,15 @@ public class Drivetrain extends SubsystemBase {
 
     public DifferentialDrive drive;
 
+    private TalonFXSensorCollection LMSensor;
+
     public Drivetrain(){
         leftMaster = new TalonFX(RobotMap.kLeftMaster); //CAN 0
         leftSlave = new TalonFX(RobotMap.kLeftSlave); //CAN 1
         rightMaster = new TalonFX(RobotMap.kRightMaster); //CAN 2
         rightSlave = new TalonFX(RobotMap.kRightSlave); //CAN 3
+
+        LMSensor = leftMaster.getSensorCollection();
 
         leftMaster.setInverted(false);
         leftSlave.setInverted(false);
@@ -31,6 +42,75 @@ public class Drivetrain extends SubsystemBase {
 
         leftSlave.follow(leftMaster); //sets slave to follow master
         rightSlave.follow(rightMaster); //sets slave to follow master
-        
+
+        //Config Slave Deadband
+        leftSlave.configNeutralDeadband(0);
+        rightSlave.configNeutralDeadband(0);
+
+        //Config NeutralMode to coast
+        leftMaster.setNeutralMode(NeutralMode.Brake);
+        rightMaster.setNeutralMode(NeutralMode.Brake);
+        leftSlave.setNeutralMode(NeutralMode.Coast);
+        rightSlave.setNeutralMode(NeutralMode.Coast);
+
+        //Configure PIDF values for Auto drive, the Left Master is the master controller for PID
+        leftMaster.config_kP(0, DrivetrainConstants.kP);
+        leftMaster.config_kI(0, DrivetrainConstants.kI);
+        leftMaster.config_kD(0, DrivetrainConstants.kD);
+        leftMaster.config_kF(0, DrivetrainConstants.kF);
+
+
     }
+
+     /**
+   * Reconfigures the motors to the drive settings
+   */
+    public void config() {
+        rightMaster.configFactoryDefault();
+        rightMaster.setInverted(true);
+        rightSlave.follow(rightMaster);
+    } 
+
+    public void stop() {
+        leftMaster.set(ControlMode.PercentOutput, 0);
+        rightMaster.set(ControlMode.PercentOutput, 0);
+      }
+      
+      public void setTank(double leftPower, double rightPower){
+        leftMaster.set(ControlMode.PercentOutput, leftPower);
+        rightMaster.set(ControlMode.PercentOutput, rightPower);
+      }
+
+      public void motionMagic (double distance, double speed,double P,double I,double D) {
+        double rotations = (distance * DrivetrainConstants.kGearRatio)/(DrivetrainConstants.kWheelDiameter*Math.PI);
+        double targetPos = rotations*2048;
+        //Convert target speed from inches / second to encoder units / 100 ms
+        double targetSpeed = (speed *DrivetrainConstants.kGearRatio * 2048 * 10) / (DrivetrainConstants.kWheelDiameter * Math.PI);
+    
+        rightSlave.follow(leftMaster);
+        rightMaster.follow(leftMaster);
+        leftMaster.configMotionCruiseVelocity((int)targetSpeed);
+        leftMaster.configMotionAcceleration((int)targetSpeed);
+        leftMaster.setSelectedSensorPosition(0);
+
+        leftMaster.config_kP(0, P);
+        leftMaster.config_kI(0, I);
+        leftMaster.config_kD(0, D);
+
+        leftMaster.set(ControlMode.MotionMagic, targetPos);
+        getPosition();
+      }
+
+      public void getPosition(){
+        SmartDashboard.putNumber("LM Position",LMSensor.getIntegratedSensorPosition());      
+    }
+
+      public double getVelocity() {
+        return leftMaster.getSelectedSensorVelocity();
+      }
+  
+      @Override
+      public void periodic() {
+        // This method will be called once per scheduler run
+      }
 }
